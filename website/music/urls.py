57913b4241e3716cc37a50e5c97fef5b69d659f0
ephraimguo <ephraimguo@gmail.com>
2017-12-13 15:16:57 +0800

    the English tut for Django

--- website/music/urls.py
+++ website/music/urls.py
@@ -1,10 +1,17 @@
+from django.conf.urls import url
 from django.urls import path, re_path
 from . import views
+
+app_name = 'music'
 
 urlpatterns = [
     # /music/  which is the music home page
     re_path(r'^$', views.index, name='index'),
 
     # /music/[id] which is the details pages for specific album
-    re_path(r'^(?P<album_id>[0-9]+)/$', views.detail, name='details')
+    re_path(r'^(?P<album_id>\d+)/$', views.detail, name='detail'),
+
+    #/music/[id]/favorite the favorite page
+    re_path(r'^(?P<album_id>\d+)/favorite$', views.favorite, name='favorite'),
+
 ]
--- website/music/views.py
+++ website/music/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
-from .models import Album
+# from django.http import Http404
+# from django.http import HttpResponse
+from .models import Album, Song
 # from django.template import loader
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 def index(request):
     all_albums = Album.objects.all() #connect to the database to get all the albums as objects!!
@@ -14,10 +16,26 @@ def index(request):
     # this command will directly picking up the file in the app/template folder, the template folder name is pl. form
 
     # template = loader.get_template('music/index.html')
-    context = {
-        'all_albums': all_albums,
-    }
-    return render(request, 'music/index.html', )
+    return render(request, 'music/index.html', {'all_albums': all_albums,})
 
 def detail(request, album_id):
-    return HttpResponse('<h2>Detail of the album id %s </h2>' %str(album_id))
+    # return HttpResponse('<h2>Detail of the album id %s </h2>' %str(album_id))
+    # try :
+    #     album = Album.objects.get(pk = album_id)
+    # except Album.DoesNotExist:
+    #     raise Http404('Album does not exist')
+
+    #shortcut for above content
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request,  'music/detail.html', {'album': album}, )
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except(KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {'album': album, 'error_message':'You did not select a valid song',})
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
